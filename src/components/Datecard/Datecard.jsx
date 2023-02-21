e8346b4761bb731d18b1eddedc8cc9c8c6a51541
taehoon619 <plok619@gmail.com>
2023-02-21 22:24:14 +0900

    timeline

--- src/components/Datecard/Datecard.jsx
+++ src/components/Datecard/Datecard.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
+import styles from "./Datecard.module.css";
 
 export default function Datecard() {
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <div className={styles.dateBoxWrap}></div>
+      <div className={styles.dateBoxWrap}>날짜 정리</div>
+      <div className={styles.dateLine}></div>
     </div>
   );
 }
-
