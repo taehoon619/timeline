7221f0697eb1c14657934eef48b5318ddb847422
taehoon619 <plok619@gmail.com>
2023-02-21 21:14:07 +0900

    timeline

---
+++ src/components/Cardbox/Cardbox.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import styles from "./Cardbox.module.css";
+
+export default function Cardbox() {
+  return (
+    <div className={styles.container}>
+      <div></div>
+      <div className={styles.cardLineWrap}>
+        <div className={styles.cardLineBoxLeft}>
+          <div className={styles.cardLine}>
+            <div className={styles.cardPoint}></div>
+          </div>
+        </div>
+        <div className={styles.cardLineBoxRight}>
+          <div className={styles.cardLine}>
+            <div className={styles.cardPoint}></div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.cardSectionWrap}>
+        <div className={styles.cardSection}>
+          <div className={styles.cardLink}>
+            <div className={styles.cardText}></div>
+          </div>
+          <div className={styles.cardGap}></div>
+          <div className={styles.left}></div>
+          <div className={styles.right}></div>
+          <div className={styles.whole}></div>
+        </div>
+        <div className={styles.cardLineTextWrap}>
+          <div className={styles.cardLineTextBox}>
+            <div className={styles.cardLineText}>
+              <div className={styles.cardText}>카드 1</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.cardLineWrap}>
+        <div className={styles.cardLineBoxLeft}>
+          <div className={styles.cardLine}>
+            <div className={styles.cardPoint}></div>
+          </div>
+        </div>
+        <div className={styles.cardLineBoxRight}>
+          <div className={styles.cardLine}>
+            <div className={styles.cardPoint}></div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.cardSectionWrap}>
+        <div className={styles.cardSection}>
+          <div className={styles.cardLink}>
+            <div className={styles.cardText}></div>
+          </div>
+          <div className={styles.cardGap}></div>
+          <div className={styles.left}></div>
+          <div className={styles.right}></div>
+          <div className={styles.whole}></div>
+        </div>
+        <div className={styles.cardLineTextWrap}>
+          <div className={styles.cardLineTextBox}>
+            <div className={styles.cardLineText}>
+              <div className={styles.cardText}>카드 2</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.cardLineWrap}>
+        <div className={styles.cardLineBoxLeft}>
+          <div className={styles.cardLine}>
+            <div className={styles.cardPoint}></div>
+          </div>
+        </div>
+        <div className={styles.cardLineBoxRight}>
+          <div className={styles.cardLine}>
+            <div className={styles.cardPoint}></div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.cardSectionWrap}>
+        <div className={styles.cardSection}>
+          <div className={styles.cardLink}>
+            <div className={styles.cardText}></div>
+          </div>
+          <div className={styles.cardGap}></div>
+          <div className={styles.left}></div>
+          <div className={styles.right}></div>
+          <div className={styles.whole}></div>
+        </div>
+        <div className={styles.cardLineTextWrap}>
+          <div className={styles.cardLineTextBox}>
+            <div className={styles.cardLineText}>
+              <div className={styles.cardText}>카드 3</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Datebox/Datebox.jsx
+++ src/components/Datebox/Datebox.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Cardbox from "../Cardbox/Cardbox";
+import Datecard from "../Datecard/Datecard";
 import styles from "./Datebox.module.css";
 
 export default function Datebox() {
   return (
     <div>
-      <div className={styles.dateLine}></div>
+      <div className={styles.dateBoxContainer}>
+        <div className={styles.dateBgLine}></div>
+      </div>
+      <Cardbox />
+      <Datecard />
     </div>
   );
 }
