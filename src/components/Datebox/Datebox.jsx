aeaaf2d35de84d50239f946e81f1fcb98e8470a2
taehoon619 <plok619@gmail.com>
2023-02-21 20:03:53 +0900

    timeline

---
+++ src/components/Datebox/Datebox.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Datebox.module.css";
+
+export default function Datebox() {
+  return (
+    <div>
+      <div className={styles.dateLine}></div>
+    </div>
+  );
+}
--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -6,36 +6,49 @@ import {
   RiArrowLeftSLine,
   RiArrowRightSLine,
 } from "react-icons/ri";
+import Datebox from "../Datebox/Datebox";
 
 export default function Timeline() {
   return (
     <div className={styles.container}>
       <div>
-        {/* 타임라인 헤더는 앱솔루트 */}
-        <div className={styles.timeLineHeader}>
-          <div className={styles.timeLineDate}>
-            <div className={styles.dateTextWrap}>
-              <div className={styles.dateIcon}>
-                <RxDoubleArrowRight />
-              </div>
-              <span>2023년 2월</span>
-              <div className={styles.TimelineBox}></div>
+        <Datebox />
+      </div>
+      {/* 타임라인 헤더는 앱솔루트 */}
+      <div className={styles.timeLineHeader}>
+        <div className={styles.timeLineDate}>
+          <div className={styles.dateTextWrap}>
+            <div className={styles.dateIcon}>
+              <RxDoubleArrowRight />
             </div>
-            <div className={styles.timeLineDayWrap}>
-              <div className={styles.TimelineBox}></div>
-              <div className={styles.monthBox}>
-                월
-                <RiArrowDownSLine className={styles.downArrowIcon} />
-              </div>
-              <div className={styles.leftIconBox}>
-                <RiArrowLeftSLine className={styles.leftArrowIcon} />
-              </div>
-              <div className={styles.toDayBox}>오늘</div>
-              <div className={styles.rightIconBox}>
-                <RiArrowRightSLine className={styles.rightArrowIcon} />
-              </div>
+            <span>2023년 2월</span>
+            <div className={styles.TimelineBox}></div>
+          </div>
+          <div className={styles.timeLineDayWrap}>
+            <div className={styles.TimelineBox}></div>
+            <div className={styles.monthBox}>
+              월
+              <RiArrowDownSLine className={styles.downArrowIcon} />
+            </div>
+            <div className={styles.leftIconBox}>
+              <RiArrowLeftSLine className={styles.leftArrowIcon} />
+            </div>
+            <div className={styles.toDayBox}>오늘</div>
+            <div className={styles.rightIconBox}>
+              <RiArrowRightSLine className={styles.rightArrowIcon} />
             </div>
           </div>
+        </div>
+      </div>
+      <div className={styles.timeLineCard}>
+        <div className={styles.cardWrap}>
+          <div className={styles.cardWidth}></div>
+          <div>
+            <div></div>
+          </div>
+          <div className={styles.cardSection}></div>
+          <div className={styles.cardSection}></div>
+          <div className={styles.cardSection}></div>
         </div>
       </div>
     </div>
