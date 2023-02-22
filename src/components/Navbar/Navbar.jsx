83bc74c60b9ed74086b0644a64ba8ef35ec521d2
taehoon619 <plok619@gmail.com>
2023-02-22 18:04:11 +0900

    timeline

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,13 +6,19 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
   return (
     <div className={styles.container}>
-      <div className={styles.timeLineText}>
-        <div className={styles.timeLineView}>
-          <CiViewTimeline />
-          <h4>타임라인 보기</h4>
-        </div>
-        <div>
-          <AiOutlinePlus />
+      <div className={styles.timeLineTextWrap}>
+        <div className={styles.timeLineText}>
+          <div className={styles.timeLine}>
+            <div className={styles.timeLineView}>
+              <di className={styles.timeLineBox}>
+                <CiViewTimeline className={styles.timeLineIcon} />
+                <h4 className={styles.lineText}>타임라인 보기</h4>
+              </di>
+            </div>
+          </div>
+          <div className={styles.contentPlusIcon}>
+            <AiOutlinePlus className={styles.contentPlus} />
+          </div>
         </div>
       </div>
       <div className={styles.menuBar}></div>
